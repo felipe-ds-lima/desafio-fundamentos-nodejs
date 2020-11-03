8a79e12c173a78c53dab06d11f6a9b4af35e99e9
Felipe D S Lima <felipe@monts.com.br>
2020-11-03 20:04:16 -0300

    challenge concluded

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (prev, curr) => prev + (curr.type === 'income' ? curr.value : 0),
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (prev, curr) => prev + (curr.type === 'outcome' ? curr.value : 0),
+      0,
+    );
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
